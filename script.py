c1fed902d082d0bc3cdb11c0d22f27bc5de2a2e8
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2026-01-03 13:07:35 +0000

    incorporate matplotlib to visualise the walk

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-# Import numpy as np
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Starting step
 step = 0
@@ -16,7 +16,7 @@ for x in range(100) :
     dice = np.random.randint(1,7)
 
     if dice <= 2 :
-        step = step - 1
+        step = max(0, step - 1) # use max to make sure step can't go below 0
     elif dice <= 5 :
         step = step + 1
     else :
@@ -26,3 +26,12 @@ for x in range(100) :
 
 # print out the new step value
 print(random_walk)
+
+# Plot the random walk using matplotlib.pyplot
+plt.plot(random_walk)
+plt.title("Simulation")
+plt.xlabel("Roll")
+plt.ylabel("Step")
+
+# display the plot
+plt.show()
