05228d3ed3febfa4da0469822f2d34eb290602dc
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2026-01-03 13:49:54 +0000

    finalise the probability calculator

--- script.py
+++ script.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# Python script to calculate the probability of reaching at least step 60 after 100 rolls of a dice
+# Start on step 0.  Roll dice and move up or down based on the dice value (1-2: down 1 step, 3-5: up 1 step, 6: roll again and go up that number of steps)
+# Include clumsiness factor (there is a 0.5% chance of falling to step 0 on each roll)
+
 # Starting step
 step = 0
 
-# Set the seed
+# Set the seed for random number generator for reproducibility.  Comment out to get different results each time
 np.random.seed(123)
 
 # clear the plot in case running multiple times
@@ -13,7 +18,7 @@ plt.clf()
 # initialise all walks
 all_walks = []
 
-for i in range(20):
+for i in range(500):
     random_walk = [0]
     # Control the steps gained/lost based upon the dice value, after 100 rolls
     for x in range(100) :
@@ -34,15 +39,19 @@ for i in range(20):
         random_walk.append(step)
     all_walks.append(random_walk)
 
-# Convert all_walks to a NumPy array
-np_aw = np.array(all_walks)
+# Convert all_walks to a NumPy array & transpose
+np_aw_t = np.transpose(np.array(all_walks))
 
-# transpose np_aw in order to visualise five random walks (all walks)
-np_aw_t = np.transpose(np_aw)
+# capture final/end positions
+ends = np_aw_t[-1,:]
+
+# Confirm the probability that you'll reach at least step 60 
+probability = np.mean(ends >= 60)
+print("Probability of reaching at least step 60 is: " + str(probability*100) + "%")
 
 # Plot all walks & show
-plt.plot(np_aw_t)
-plt.title("20 Random Walks of 100 Steps Each")
+plt.hist(ends)
+plt.title("Distribution: 500 Random Walks of 100 Steps Each")
 plt.xlabel("Roll Number")
 plt.ylabel("Position")
 plt.show()
