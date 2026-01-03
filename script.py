739c1db6cf00afbf9207c555d732d2ecc9845eb8
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2026-01-03 13:24:48 +0000

    Visualise five random walks

--- script.py
+++ script.py
@@ -7,31 +7,33 @@ step = 0
 # Set the seed
 np.random.seed(123)
 
-# initialise a random walk
-random_walk = [0]
+# initialise all walks
+all_walks = []
 
-# Control the steps gained/lost based upon the dice value, after 100 rolls
-for x in range(100) :
-    step = random_walk[-1]
-    dice = np.random.randint(1,7)
+for i in range(5):
+    random_walk = [0]
+    # Control the steps gained/lost based upon the dice value, after 100 rolls
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
 
-    if dice <= 2 :
-        step = max(0, step - 1) # use max to make sure step can't go below 0
-    elif dice <= 5 :
-        step = step + 1
-    else :
-        step = step + np.random.randint(1,7)
+        if dice <= 2 :
+            step = max(0, step - 1) # use max to make sure step can't go below 0
+        elif dice <= 5 :
+            step = step + 1
+        else :
+            step = step + np.random.randint(1,7)
+        
+        random_walk.append(step)
     
-    random_walk.append(step)
+    all_walks.append(random_walk)
 
-# print out the new step value
-print(random_walk)
+# Convert all_walks to a NumPy array
+np_aw = np.array(all_walks)
 
-# Plot the random walk using matplotlib.pyplot
-plt.plot(random_walk)
-plt.title("Simulation")
-plt.xlabel("Roll")
-plt.ylabel("Step")
+# transpose np_aw in order to visualise five random walks
+np_aw_t = np.transpose(np_aw)
 
-# display the plot
+# Plot all walks & show
+plt.plot(np_aw_t)
 plt.show()
