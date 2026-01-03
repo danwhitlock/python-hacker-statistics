eeef66dbb3c8da045cd585ed22e3609b7e50014e
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2026-01-03 13:00:06 +0000

    incorporate random walk

--- script.py
+++ script.py
@@ -2,24 +2,27 @@
 import numpy as np
 
 # Starting step
-step = 50
+step = 0
 
 # Set the seed
 np.random.seed(123)
 
-# Use randint() to simulate a dice
-dice = np.random.randint(1,7)
-print("Rolling the dice...")
-print("You rolled a " + str(dice))
+# initialise a random walk
+random_walk = [0]
 
-# Control the steps gained/lost based upon the dice value
+# Control the steps gained/lost based upon the dice value, after 100 rolls
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
 
-if dice <= 2 :
-    step = step - 1
-elif dice <= 5 :
-    step = step + 1
-else :
-    step = step + np.random.randint(1,7)
+    if dice <= 2 :
+        step = step - 1
+    elif dice <= 5 :
+        step = step + 1
+    else :
+        step = step + np.random.randint(1,7)
+    
+    random_walk.append(step)
 
 # print out the new step value
-print("You are now on step " + str(step))
+print(random_walk)
